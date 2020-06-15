48847ed2c54aedbff2ab1ea927ca917004b5cb3d
Cosmin <co5myn92@yahoo.com>
2020-06-15 17:15:43 +0300

    search impementation

--- app.js
+++ app.js
@@ -1,8 +1,19 @@
 const auth = '563492ad6f91700001000001e3ddaf3f2c2445c58e3f9b0fd1b13b3a';
 const gallery = document.querySelector('.gallery');
 const searchInput = document.querySelector('.search-input');
-const submitButton = document.querySelector('.submit-btn');
+const form = document.querySelector('.search-form');
 let searchValue;
+
+// Event Listeners
+searchInput.addEventListener('input', updateInput);
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  searchPhotos(searchValue);
+});
+
+function updateInput(e) {
+  searchValue = e.target.value;
+}
 
 async function curatedPhotos() {
   const dataFetch = await fetch('https://api.pexels.com/v1/curated', {
@@ -12,6 +23,30 @@ async function curatedPhotos() {
       Authorization: auth,
     },
   });
+  const data = await dataFetch.json();
+  data.photos.forEach((photo) => {
+    //console.log(photo);
+    const galleryImg = document.createElement('div');
+    galleryImg.classList.add('gallery-img');
+    galleryImg.innerHTML = `<img src=${photo.src.medium}></img>
+    <p>${photo.photographer}</p
+    `;
+    // AppendChild
+    gallery.appendChild(galleryImg);
+  });
+}
+
+async function searchPhotos(query) {
+  const dataFetch = await fetch(
+    `https://api.pexels.com/v1/search?query=${query}`,
+    {
+      method: 'GET',
+      headers: {
+        Accept: 'application/json',
+        Authorization: auth,
+      },
+    }
+  );
   const data = await dataFetch.json();
   data.photos.forEach((photo) => {
     console.log(photo);
@@ -24,5 +59,4 @@ async function curatedPhotos() {
     gallery.appendChild(galleryImg);
   });
 }
-
 curatedPhotos();

679d980ce31b7d769e6579112d47fdcd599c88f8
Cosmin <co5myn92@yahoo.com>
2020-06-16 13:12:48 +0300

    search clear

--- app.js
+++ app.js
@@ -29,11 +29,15 @@ async function fetchApi(url) {
 
 function generatePictures(data) {
   data.photos.forEach((photo) => {
+    console.log(photo);
     const galleryImg = document.createElement('div');
     galleryImg.classList.add('gallery-img');
     galleryImg.innerHTML = `
-    <img src=${photo.src.medium}></img>
+    <div class='gallery-info'>
     <p>${photo.photographer}</p
+    <a href=${photo.src.original}>Download</a>
+    </div>
+    <img src=${photo.src.portrait}></img>
     `;
     gallery.appendChild(galleryImg);
   });
@@ -48,9 +52,15 @@ async function curatedPhotos() {
 }
 
 async function searchPhotos(query) {
+  clear();
   const data = await fetchApi(
     `https://api.pexels.com/v1/search?query=${query}+query&per_page=15&page=1`
   );
   generatePictures(data);
+}
+
+function clear() {
+  gallery.innerHTML = '';
+  searchInput.value = '';
 }
 curatedPhotos();
